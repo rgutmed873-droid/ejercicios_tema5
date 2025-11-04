771328037559b39daf62d7d3e05ae918faf7c0d7
rafag <rgutmed873@iesgrancapitan.org>
2025-11-04 17:30:27 +0100

    Ejercicio3 terminado y todos hechos del tema 5

--- Ejercicios/src/ejercicios/altos/ejercicio3_altos_tema5.java
+++ Ejercicios/src/ejercicios/altos/ejercicio3_altos_tema5.java
@@ -20,7 +20,43 @@ public class ejercicio3_altos_tema5 {
                 int num2 = sc.nextInt();
                 double hipotenusa = (Math.pow(num1,2) + Math.pow(num2,2));
                 System.out.println("La longitud de la hipotenusa es" + " " + hipotenusa);
+                break;
+            case 2:
+                System.out.println("Introduce el valor del radio");
+                int num3 = sc.nextInt();
+                int pi = (int) Math.PI;
+                double superficie = (Math.pow(num3,2)*pi);
+                System.out.println("El valor de la superficie es" + " " + superficie);
+                break;
+            case 3:
+                System.out.println("Introduce el valor del radio");
+                int num4 = sc.nextInt();
+                pi = (int) Math.PI;
+                double perimetro = (2*(Math.pow(num4,2))*pi);
+                System.out.println("El valor del perimtero es" + " " + perimetro);
+                break;
+            case 4:
+                System.out.println("Introduce el valor de la base");
+                int base = sc.nextInt();
+                System.out.println("Introduce el valor de la altura");
+                int altura = sc.nextInt();
+                int area = base*altura;
+                System.out.println("El valor del area del rectangulo es" + " " + area);
+                break;
+            case 5:
+                System.out.println("Introduce el valor de la base");
+                int base2 = sc.nextInt();
+                System.out.println("Introduce el valor de la altura");
+                int altura2 = sc.nextInt();
+                int areaTriangulo = (base2*altura2)/2;
+                System.out.println("El valor del area del tríangulo es" + " " + areaTriangulo);
+                break;
         }
-       
+
+        if (opcion<=0){
+            System.out.println("Opción incorrecta");
+        }else if (opcion>5){
+            System.out.println("Opción incorrecta");
+        }
     }
 }
